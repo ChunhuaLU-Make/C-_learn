bc79c60ab93ba9fafd4f3a5a8b46c6210642965b
ChunhuaLU-Make <1692470640@qq.com>
2020-07-21 22:06:52 +0800

    构造 析构
    构造函数的使用，跟this的使用，析构的调用时机

---
+++ object/Two/ConsoleApplication1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClassName xiaoHua = new ClassName("WangKing", 12);
+            xiaoHua.Say();
+
+            Student She = new Student();
+            She.Name = "LongShan";
+            She.Sex = 1;
+            She.Eag = 95;
+            She.Say();
+            while (true) ;  //并未输出xigou，代表程序结束才调用析构，并不是对象使用完就释放类。
+        }
+    }
+}
